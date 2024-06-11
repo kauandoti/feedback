32c9a2c47573aa6e3769bce2a4b6fd4914953b13
kauan felix <kauanfelix10.a@gmail.com>
2024-06-11 02:16:06 +0000

    feat: instalaçao e implantaçao do react-router-dom e do style-components

---
+++ src/styles/theme.js
@@ -0,0 +1,10 @@
+export const theme = {
+    bomeBackground: "#131518",
+    cardGradient: "radial-gradient(98.96% 98.96% at 50% 0%, #232A34 0%, #181E27 100%)" ,
+    textColor: "#969FAD",
+    submitButtonColor: "#FC7614",
+    white: "#FFF",
+    feedBackButtonColor: "#262E38",
+    feedBackButtonText: "#7C8798"
+
+}
