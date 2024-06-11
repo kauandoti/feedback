353ad812fb1f1736e058b552a77e6c402a885795
kauan felix <kauanfelix10.a@gmail.com>
2024-06-11 14:23:03 +0000

    adicao da verificacao da nota e do envio dta para mudar de pagina

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,41 +1,57 @@
 import { CardConteiner, IconContainer, ButtonContainer, ResultContainer } from "./Home.styles";
-
 import iconStarImg from "../assets/icon-star.svg"
 import thankYouImg from "../assets/illustration-thank-you.svg"
+import { useState } from "react";
+import styled from "styled-components";
 
-export function  Home() {
-  let aparecerResultado = true
+
+export function Home() {
+  const [mostrarResultado, setMostrarResultado] = useState(false);
+  const [feedbackNote, setFeedbackNote] = useState(0);
+  const [selectedFeedback, setSelectedFeedback] = useState(null); 
+
+  function handleFeedBackButtonClick(event) {
+    const feedback = parseInt(event.target.innerText, 10);
+    setFeedbackNote(feedback);
+    setSelectedFeedback(feedback); 
+  }
+
+  function handleSubmit() {
+    if (feedbackNote !== 0) {
+      setMostrarResultado(true);
+    }
+  }
+
   return (
-    aparecerResultado === false ? (
+    mostrarResultado === false ? (
       <CardConteiner>
         <IconContainer>
-          <img src="{iconStarImg}" alt="icone de estrela" />
+          <img src={iconStarImg} alt="icone de estrela" />
         </IconContainer>
 
         <h1>Como foi o atendimento?</h1>
         <p>Conte-nos como foi nossa ajuda com sua solicitação. Agradecemos muito seu feedback para podermos melhorar nosso atendimento!</p>
 
         <ButtonContainer>
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>4</button>
-          <button>5</button>
+          <button onClick={handleFeedBackButtonClick} className={selectedFeedback === 1 ? 'selected' : ''}>1</button>
+          <button onClick={handleFeedBackButtonClick} className={selectedFeedback === 2 ? 'selected' : ''}>2</button>
+          <button onClick={handleFeedBackButtonClick} className={selectedFeedback === 3 ? 'selected' : ''}>3</button>
+          <button onClick={handleFeedBackButtonClick} className={selectedFeedback === 4 ? 'selected' : ''}>4</button>
+          <button onClick={handleFeedBackButtonClick} className={selectedFeedback === 5 ? 'selected' : ''}>5</button>
         </ButtonContainer>
-        <button>Enviar</button>
+        <button onClick={handleSubmit}>Enviar</button>
       </CardConteiner>
     ) : (
       <CardConteiner>
         <img src={thankYouImg} alt="imagem de agradecimento" />
         <ResultContainer>
-          <p>Você selecionou 4 de 5</p>
-          </ResultContainer>
+          <p>Você selecionou {feedbackNote} de 5</p>
+        </ResultContainer>
 
-          <h1>Obrigado</h1>
+        <h1>Obrigado</h1>
 
-          <p>Agradecemos por dedicar um momento para nos avaliar. Se precisar de mais suporte, não hesite em entrar em contato!</p>
+        <p>Agradecemos por dedicar um momento para nos avaliar. Se precisar de mais suporte, não hesite em entrar em contato!</p>
       </CardConteiner>
     )
   )
 }
-
